9136bab47ab7872adb450520d9c78cd5c435f497
Szymon Kubiak <skub@thaumatec.com>
2024-05-22 23:54:56 +0200

    index

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,3 @@
-import { readFile } from "./utils";
-import { Song } from "./structures/text";
-import { SongDivider } from "./song-divider";
-
-// export { Song } from "./structures/text";
-// export { SongDivider } from "./song-divider";
-
-// const song = new Song(readFile("src/examples/song_format.txt"));
-
-// const songDivider = new SongDivider(20, 5, song);
-
-// const parts = songDivider.getNumberOfParts();
-// const pages = songDivider.getPagesForPart(0);
-// const a = songDivider.getDisplayPageForPart(0, 0);
-
 export { ParsedTextUnit } from "./parser/parsed-text-unit";
 export { ParsedTextUnitPart } from "./parser/parsed-text-unit-part";
 
