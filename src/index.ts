d02638f9925be3e10a527c490913d2d56ec942a8
Szymon Kubiak <skub@thaumatec.com>
2024-05-23 10:26:18 +0200

    ordered export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { ParsedTextUnit } from "./parser/parsed-text-unit";
 export { ParsedTextUnitPart } from "./parser/parsed-text-unit-part";
+export { OrderedParsedTextUnit } from "./parser/ordered-text-unit-parts";
 
 export { LinesWrapper } from "./wrapper/lines-wrapper";
 export { LinesWrapperPaginator } from "./wrapper/lines-wrapper-paginator";
